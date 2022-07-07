3ccef6e7591f4769973caad93d173b4c59c228f6
Joe McGarry <joewmcgarry@gmail.com>
2022-07-07 15:24:31 +0100

    Creates createNote method and tests. Currently not working

--- notesApi.js
+++ notesApi.js
@@ -6,6 +6,21 @@ class NotesApi {
         callback(data)
       });
   }
+
+  createNote(note) {
+    const data = { noteMessage: note };
+    fetch('http://localhost:3000/notes', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+        },
+      body: JSON.stringify(data),
+    })  
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+  }
 }
 
 module.exports = NotesApi;
--- notesApi.test.js
+++ notesApi.test.js
@@ -1,31 +1,36 @@
 const NotesApi = require('./Notesapi');
 
-// This makes `fetch` available to our test
-// (it is not by default, as normally `fetch` is only
-// available within the browser)
 require('jest-fetch-mock').enableMocks()
 
 describe('API class', () => {
   it('calls fetch and loads data', () => {
-    // 1. Instantiate the class
     const api = new NotesApi();
 
-    // 2. We mock the response from `fetch`
-    // The mocked result will depend on what your API
-    // normally returns — you want your mocked response
-    // to "look like" as the real response as closely as
-    // possible (it should have the same fields).
     fetch.mockResponseOnce(JSON.stringify({
       name: "Some value",
       id: 123
     }));
 
-    // 3. We call the method, and use `expect`
-    // to assert the values we get back contain
-    // what we expect.
     api.loadData((returnedDataFromApi) => {
       expect(returnedDataFromApi.name).toBe("Some value");
       expect(returnedDataFromApi.id).toBe(123);
     });
   });
+    
+    
+  describe('createNote()', () => {
+    it('POSTs a note to server', () => {
+      const api = new NotesApi();
+      fetch.mockResponseOnce(JSON.stringify({
+        noteMessage: "Some value"
+      }));
+
+      api.createNote("Some value");
+
+      // api.loadData((returnedDataFromApi) => {
+      //   expect(returnedDataFromApi.noteMessage).toBe("Some value");
+      // });
+
+    })
+  })
 });
