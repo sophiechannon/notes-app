685d96f0cae5d6a23d064ea561c0c63eee047b93
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-07 19:21:14 +0100

    original tests now working

--- NotesView.test.js
+++ NotesView.test.js
@@ -5,51 +5,58 @@
 const fs = require('fs');
 const NotesView = require('./NotesView')
 const NotesModel = require('./notesModel');
-const NotesApi = require('./NotesApi');
 
+describe('displayNotes', () => {
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+  });
 
-// describe('displayNotes', () => {
-//   beforeEach(() => {
-//     document.body.innerHTML = fs.readFileSync('./index.html');
-//   });
-
-//   it('it displays notes from the notesModel', () => {
-//     const model = new NotesModel();
-//     const api = new NotesApi();
-//     const view = new NotesView(model, api) ;
-//     const messageBox = document.querySelector('#message-input')
-//     messageBox.value = 'wash the car';
-//     const button = document.querySelector('#add-note-button');
-//     button.click();
+  it('it displays notes from the notesModel', () => {
+    const model = new NotesModel();
+    const api = {
+      loadData: () => {
+        model.setNotes(['Joe I made it work!']);
+        view.displayNotes();
+      },
+      createNote: () => {
+        return { content: 'Joe I made it work!' }
+      }
+    }
+    const view = new NotesView(model, api) ;
+    const messageBox = document.querySelector('#message-input')
+    messageBox.value = 'wash the car';
+    const button = document.querySelector('#add-note-button');
+    button.click();
     
-//     expect(document.querySelectorAll('div.note').length).toEqual(1);
-//     expect(document.querySelectorAll('div.note')[0].innerText).toEqual('wash the car');
-//   })
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual('wash the car');
+  })
 
-//   it('displays correct number of notes when two notes are added', () => {
-//     const model = new NotesModel();
-//     const api = {
-//       loadData: () => {
-//         model.setNotes(['FUCK THIS CHAPTER']);
-//         view.displayNotes();
-//       },
-//       createNote: () => {
-//     }
-
-//     const view = new NotesView(model, api);
-//     const messageBox = document.querySelector('#message-input')
-//     messageBox.value = 'wash the car';
-//     const button = document.querySelector('#add-note-button');
-//     button.click();
-//     messageBox.value = 'walk the dog';
-//     button.click();
+  it('displays correct number of notes when two notes are added', () => {
+    const model = new NotesModel();
+    const api = {
+      loadData: () => {
+        model.setNotes(['Joe I made it work!']);
+        view.displayNotes();
+      },
+      createNote: () => {
+        return { content: 'Joe I made it work!' }
+      }
+    }
+    const view = new NotesView(model, api);
+    const messageBox = document.querySelector('#message-input')
+    messageBox.value = 'wash the car';
+    const button = document.querySelector('#add-note-button');
+    button.click();
+    messageBox.value = 'walk the dog';
+    button.click();
     
-//     expect(document.querySelectorAll('div.note').length).toEqual(2);
-//     expect(document.querySelectorAll('div.note')[0].innerText).toEqual('wash the car');
-//     expect(document.querySelectorAll('div.note')[1].innerText).toEqual('walk the dog');
-//     expect(messageBox.value).toEqual("");
-//   })
-// })
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual('wash the car');
+    expect(document.querySelectorAll('div.note')[1].innerText).toEqual('walk the dog');
+    expect(messageBox.value).toEqual("");
+  })
+})
 
 describe('displayNotesFromApi', () => {
   beforeEach(() => {
@@ -59,7 +66,7 @@ describe('displayNotesFromApi', () => {
   it('it displays notes from the API', () => {
     const mockedApi = {
       loadData: () => {
-        model.setNotes(['FUCK THIS CHAPTER']);
+        model.setNotes(['Joe I made it work!']);
         view.displayNotes();
       }
     }
@@ -67,6 +74,6 @@ describe('displayNotesFromApi', () => {
     const view = new NotesView(model, mockedApi);
     view.displayNotesFromApi();
     expect(document.querySelectorAll('div.note').length).toEqual(1);
-    expect(document.querySelectorAll('div.note')[0].innerText).toEqual("FUCK THIS CHAPTER");
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual("Joe I made it work!");
   })
 })
